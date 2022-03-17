3b9f9746717684a8c1aec412392302dc48d5deb4
Peter Bishop <pjb.den@gmail.com>
2022-03-17 16:46:58 -0600

    added directory that was untracked

---
+++ ui/src/pages/Home.js
@@ -0,0 +1,21 @@
+function Home() {
+  return <div>HOME PAGE</div>;
+}
+
+export default Home;
+
+// LIST FILTERS (optional)
+
+// SEARCH bar
+
+// NEW entity button (CREATE)
+//- [ ] A POST request to `/entity` creates a new entity
+
+// LIST of documents/entries (READ)
+
+// - EDIT button (UPDATE)
+//- [ ] A GET request to `/entity/:id` returns the given entity, or a 404 status code if that entity does not exist.
+
+// - DELETE button (DELETE)
+// - [ ] A DELETE request to `/entity/:id` deletes the given entity, or a 404 status code if that entity does not exist.
+// - [ ] A 200 status code, with a message of 'ok' or something similar is returned upon successful deletion.
