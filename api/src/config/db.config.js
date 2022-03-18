d6c9b4e266f2de19d5915bd8d8109c036780763e
Peter Bishop <pjb.den@gmail.com>
2022-03-18 00:59:43 -0600

    details page displays all attributes of dog

--- api/src/config/db.config.js
+++ api/src/config/db.config.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//   url: "mongodb://localhost:27017/doggie_db",
-// };
-
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/doggie_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
+module.exports = {
+  url: "mongodb://localhost:27017/doggie_db",
+};
--- ui/src/pages/Details.js
+++ ui/src/pages/Details.js
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 function Details() {
-  return <div>details!</div>;
+  const { id: useParam } = useParams();
+  const [dog, setDog] = useState({
+    id: useParam,
+    name: "",
+    age: "",
+    weight: "",
+    color: "",
+    breed: "",
+  });
+
+  useEffect(() => {
+    const doggyDetails = async () => {
+      try {
+        const res = await fetch(`/dog/${useParam}`);
+        const data = await res.json();
+        console.log(data);
+        setDog(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    doggyDetails();
+  }, []);
+
+  return (
+    <div>
+      <h3>{dog.name}</h3>
+      <h3>{dog.age}</h3>
+      <h3>{dog.weight}</h3>
+      <h3>{dog.color}</h3>
+      <h3>{dog.breed}</h3>
+      <Link to={`/`}>BACK</Link>
+    </div>
+  );
 }
 
 export default Details;
